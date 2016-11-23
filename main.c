ee69407c47c6c4ee9a5ddded448796faaa1db320
Federico <fedemonte.95@gmail.com>
2016-11-23 09:07:37 -0600

    procesos

--- main.c
+++ main.c
@@ -5,17 +5,13 @@
 
 int get_cantidad_vertices(char pfilename[]){
 	FILE* fp;
-
 	char single_line[150];
 	char cant_nodos_char[4];
-
 	int renglones = 0;
 	int index;
 	int cant_nodos_index;
 	int vertices;
-
 	fp = fopen(pfilename, "r");
-
 	while(!feof(fp)){
 		fgets(single_line, 150, fp);
 			index = 0;
@@ -39,48 +35,38 @@ int get_cantidad_vertices(char pfilename[]){
 	fclose(fp);
 	return(vertices);
 }
-void matriz_adyacencia(){
-
-}
 
 int main(int argc, char* argv[]){
-	/*
-	int filas = 1024;
-	int conlumnas = 1024;
-	int matrix[filas][conlumnas];
-	int i, j;
-	//llenar la matriz de ceros
-		for(j=1; j<1024; j++){
-			matrix[i][j] = 0;
-			//printf("%d ", matrix[i][j]); // imprime elemento de matriz
-		}
-	
-	*/
-
-	FILE* fp;
-	char* filename;
-	char single_line[150];
-
-	char num_char[4];
-	char num2_char[4];
-	int num2_int;
-	int num_int;
-	int number_index;
-
-	int cant_vertices;
-	int renglones;
-	int index;
-
+	FILE* 	fp;
+	char* 	filename;
+	char 	single_line[150];
+	char 	num_char[4];
+	char 	num2_char[4];
+	int 	num2_int;
+	int 	num_int;
+	int 	number_index;
+	int 	cant_vertices;
+	int 	renglones;
+	int 	index;
+	int 	i, j;
 	if(argc < 2){
 		printf("Missing filename\n");
 		exit(-1);
 		return 1;
-
 	} else {
 		filename = argv[1];
 		cant_vertices = get_cantidad_vertices(filename);
+		//llenar la matriz de ceros
 		//printf(" Cantidad de vertices %d\n", cant_vertices);
 	}
+
+	int matrix[cant_vertices][cant_vertices];
+	for(i=0; i <cant_vertices; i++){
+		for(j=0; j<cant_vertices; j++){
+		matrix[i][j] = 0;
+		}
+	}
+	
 	fp = fopen(filename, "r"); // Opens file in read-only mode
 	if(fp){ // if the file opens succesfully, then prints contents
 		int renglon = 0;
@@ -101,11 +87,7 @@ int main(int argc, char* argv[]){
 					}
 					num_int = atoi(num_char);  
 					// ahora a buscar el segundo numero de la tupla
-					
-					//printf("%d\n", num_int);
-					//printf("%c\n", single_line[index]);
 					index++;
-
 					number_index = 0;
 					strcpy(num2_char, " ");
 					num2_char[3] = ' ';
@@ -113,22 +95,24 @@ int main(int argc, char* argv[]){
 						num2_char[number_index] = single_line[index];
 						index++;
 						number_index++;
-
 					}
 					num2_int = atoi(num2_char);
-					printf("%d\n", num2_int);
-					// printf(" ultimo %c\n", single_line[index]); // imprime )
+					matrix[num_int][num2_int] = 1;
+					//printf("%d\n", num2_int);
+					
 				}// end of if 
 				index++;
 			}
 			renglon++;
+		} // while principal 
+		for(i=0; i <cant_vertices; i++){
+			for(j=0; j<cant_vertices; j++){
+				printf("%d, ", matrix[i][j]);
+			}
+			printf("\n");
 		}
-
 	} else {
 		printf("Failed to open the file.\n");
 	} // end of else failed to open file  
 	return(0);
 }
-
-
-
